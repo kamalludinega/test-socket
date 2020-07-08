6b5e98f387b76b3b5b2763c09320a253ce3ed80c
Ega Kamalludin <kamalludinega@gmail.com>
2020-07-08 16:39:54 +0700

    test socket

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/testsocket', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+app.post('/api/testsocket', function (req, res) {
+  console.log(req.body.data);
+  const data = req.body.data
+  if (io) {
+    io.emit('testsocket', data);
+    return res.json({ error: false, data })
+  }
+  return res.json({ error: true, data })
 });
 
-http.listen(port, function(){
+io.on('connection', function (socket) {
+  console.log('user connected')
+});
+
+http.listen(port, function () {
   console.log('listening on *:' + port);
 });
